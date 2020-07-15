62ced987e06d1604ea58679230f27fcd61291b93
lf- <lf-@users.noreply.github.com>
2020-07-14 22:13:14 -0700

    scaffolding

---
+++ test.nix
@@ -0,0 +1,12 @@
+#{parm1, parm2}:
+{
+  /* This is a function blah blah
+     with a long ass doc comment
+   */
+   the-fn = a: b: {z = a; y = b;};
+
+   # this one
+   # has multiple
+   # comments
+   the-snd-fn = {b, /* doc */ c}: {};
+ }
