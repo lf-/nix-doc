0216463341e194bc28ccccbf96507914206b405e
Jade <software@lfcode.ca>
2021-09-08 02:17:01 -0700

    demo port to nightly rust

--- default.nix
+++ default.nix
@@ -2,31 +2,48 @@ let
   sources = import ./nix/sources.nix;
 in
 { nixpkgs ? import <nixpkgs> }:
-let
-  pkgs = nixpkgs { };
-  inherit (import sources.gitignore { inherit (pkgs) lib; }) gitignoreSource;
-in
-pkgs.rustPlatform.buildRustPackage {
-  pname   = "nix-doc";
-  version = "0.5.1";
+  let
+    nixpkgs-mozilla = import sources.nixpkgs-mozilla;
+    pkgs = nixpkgs {
+      overlays = [
+        nixpkgs-mozilla
+        (
+          final: prev:
+            let
+              nightlyRust = final.latest.rustChannels.nightly.rust;
+            in
+              {
+                rustPlatform = prev.makeRustPlatform {
+                  rustc = nightlyRust;
+                  cargo = nightlyRust;
+                };
+              }
+        )
+      ];
+    };
+    inherit (pkgs.nix-gitignore) gitignoreSource;
+  in
+    pkgs.rustPlatform.buildRustPackage {
+      pname = "nix-doc";
+      version = "0.5.1";
 
-  cargoSha256 = "11bcn111j4iqwqwpp26w7rnycasbhiixaqb1rq4ry2402hvl90j7";
+      cargoSha256 = "11bcn111j4iqwqwpp26w7rnycasbhiixaqb1rq4ry2402hvl90j7";
 
-  src = gitignoreSource ./.;
+      src = gitignoreSource [] ./.;
 
-  nativeBuildInputs = with pkgs; [
-    pkg-config
-  ];
+      nativeBuildInputs = with pkgs; [
+        pkg-config
+      ];
 
-  buildInputs = with pkgs; [
-    boost
-    nix
-  ];
+      buildInputs = with pkgs; [
+        boost
+        nix
+      ];
 
-  # meta = with nixpkgs.stdenv.lib; {
-  #   description = "A source-based Nix documentation tool";
-  #   homepage    = "https://github.com/lf-/nix-doc";
-  #   license     = licenses.lgpl3Plus;
-  #   platforms   = platforms.all;
-  # };
-}
+      # meta = with nixpkgs.stdenv.lib; {
+      #   description = "A source-based Nix documentation tool";
+      #   homepage    = "https://github.com/lf-/nix-doc";
+      #   license     = licenses.lgpl3Plus;
+      #   platforms   = platforms.all;
+      # };
+    }
